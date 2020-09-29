01540fa7cf1657fbe962a6c90159c8de2828ee40
Zach Drummond <zachdrummond3@gmail.com>
2020-09-29 14:50:11 -0400

    Branch for Class

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const UserInput = require("./lib/UserInput");
 const path = require("path");
 const fs = require("fs");
 
@@ -9,52 +10,92 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+let memberType = "";
 
-const roleArray = ["manager", "engineer", "intern"];
-const questions = [
-  "What is your manager's office number?",
-  "What is your engineer's GitHub username?",
-  "What is your intern's school?",
-];
+function memberType(){
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "teamMember",
+      message: "Which type of team member would you like to add?",
+      choices: ["Manager", "Engineer", "Intern", "None"],
+      default: "Manager"
+    }
+  ]).then(response =>{
+    memberType = response.teamMember;
 
-inquirer
-  .prompt([
+  }).catch(error => {
+    if (error) throw error;
+  });
+}
+
+function createTeam(){
+  let counter = 0;
+  const roleArray = ["manager", "engineer", "intern"];
+  const questions = [
+      "What is your manager's office number?",
+      "What is your engineer's GitHub username?",
+      "What is your intern's school?",
+    ];
+
+  inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: `What is your ${roleArray[0]}'s name?`,
+      message: `What is your ${roleArray[counter]}'s name?`,
       default: "No name given.",
     },
     {
       type: "number",
       name: "id",
-      message: `What is your ${roleArray[0]}'s id?`,
+      message: `What is your ${roleArray[counter]}'s id?`,
       default: "No id given.",
     },
     {
       type: "input",
       name: "email",
-      message: `What is your ${roleArray[0]}'s email address?`,
+      message: `What is your ${roleArray[counter]}'s email address?`,
       default: "No email address given.",
     },
     {
       type: "input",
-      name: "officeNumber",
-      message: questions[0],
-    },
-  ])
-  .then(function (response) {
-    const {name, id, email, officeNumber} = response;
-    const manager = new Manager(name, id, email, officeNumber);
-    manager.getName();
-    manager.getId();
-    manager.getEmail();
-    manager.getOfficeNumber();
-    manager.getRole();
-  })
-  .catch(function (error) {
+      name: "special",
+      message: questions[counter],
+    }
+  ]).then(response =>{
+    
+
+  }).catch(error => {
     if (error) throw error;
   });
+}
+
+      ])
+      .then(function (response) {
+        const { name, id, email, special, teamMember } = response;
+
+        if ((counter === 0)) {
+          const manager = new Manager(name, id, email, special);
+        }
+
+        if (teamMember === "Engineer") {
+          counter = 1;
+          const engineer = new Engineer(name, id, email, special);
+
+        } else if (teamMember === "Intern") {
+          counter = 2;
+          const intern = new Intern(name, id, email, special);
+
+        } else {
+          console.log("Team Roster Complete.");
+          console.log(manager, engineer, intern);
+        }
+      })
+      .catch(function (error) {
+        if (error) throw error;
+      });
+  }
+}
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -72,9 +113,3 @@ inquirer
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```

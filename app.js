4026fedc60d07ba5a579ab3eb68bd528334f2549
Zach Drummond <zachdrummond3@gmail.com>
2020-09-29 15:26:34 -0400

    Switched to funcitons and added teamArray

--- app.js
+++ app.js
@@ -7,57 +7,91 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
+const teamArray = [];
 
-const roleArray = ["manager", "engineer", "intern"];
-const questions = [
-  "What is your manager's office number?",
-  "What is your engineer's GitHub username?",
-  "What is your intern's school?",
-];
+function createMemberType() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "teamMember",
+        message: "Which type of team member would you like to add?",
+        choices: ["Manager", "Engineer", "Intern", "None"],
+        default: "Manager",
+      },
+    ])
+    .then((response) => {
+      if(response.teamMember === "None"){
+        console.log("Team Roster Complete.");
+      } else{
+        createTeam(response.teamMember);
+      }
+    })
+    .catch((error) => {
+      if (error) throw error;
+    });
+}
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "name",
-      message: `What is your ${roleArray[0]}'s name?`,
-      default: "No name given.",
-    },
-    {
-      type: "number",
-      name: "id",
-      message: `What is your ${roleArray[0]}'s id?`,
-      default: "No id given.",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: `What is your ${roleArray[0]}'s email address?`,
-      default: "No email address given.",
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: questions[0],
-    },
-  ])
-  .then(function (response) {
-    const {name, id, email, officeNumber} = response;
-    const manager = new Manager(name, id, email, officeNumber);
-    manager.getName();
-    manager.getId();
-    manager.getEmail();
-    manager.getOfficeNumber();
-    manager.getRole();
-  })
-  .catch(function (error) {
-    if (error) throw error;
-  });
+function createTeam(memberType) {
+  let counter = 0;
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+  if(memberType === "Engineer"){
+    counter = 1;
+  } else if(memberType === "Intern"){
+    counter = 2;
+  }
+
+  const questions = [
+    "What is your manager's office number?",
+    "What is your engineer's GitHub username?",
+    "What is your intern's school?",
+  ];
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: `What is your ${memberType}'s name?`,
+        default: "No name given.",
+      },
+      {
+        type: "number",
+        name: "id",
+        message: `What is your ${memberType}'s id?`,
+        default: "No id given.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: `What is your ${memberType}'s email address?`,
+        default: "No email address given.",
+      },
+      {
+        type: "input",
+        name: "special",
+        message: questions[counter],
+      },
+    ])
+    .then((response) => {
+      const { name, id, email, special} = response;
+
+      if (memberType === "Manager") {
+        teamArray.push(new Manager(name, id, email, special));
+      } else if(memberType === "Engineer"){
+        teamArray.push(new Engineer(name, id, email, special));
+      } else if(memberType === "Intern"){
+        teamArray.push(new Intern(name, id, email, special));
+      }
+      createMemberType();
+    })
+    .catch((error) => {
+      if (error) throw error;
+    });
+}
+
+createMemberType();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
